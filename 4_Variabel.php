b0374cecbf86ced45c09fbb1f1f38427c9997105
anfi3281 <anfi3281@gmail.com>
2023-01-20 11:34:55 +0700

    tipe data

---
+++ 4_Variabel.php
@@ -0,0 +1,9 @@
+<?php
+$nama = "Randi Afif";
+$umur = 20;
+echo $nama;
+echo "<br/>";
+echo $umur;
+echo "<br/>";
+echo "hai, perkenalkan nama saya ".$nama." dan saya berumur ".$umur;
+?>
---
+++ 5_Tipe Data.php
@@ -0,0 +1,14 @@
+<?php
+$tes = "Saya pergi sekolah";
+echo $tes;
+$bilangan_pertama = 1;
+$bilangan_kedua = 89;
+// Kedua variabel di atas merupakan variabel yang bertipe data integer
+$angka = 12.122;
+// variabel di atas adalah variabel yang bertipe data float
+$x = false;
+$y = true;
+// variabel di atas bertipe data boolean karena berisi nilai benar atau salah
+$anggota = array("Budi", "Diah", "Ayu");
+// variabel anggota di atas adalah array
+echo "<br/>".$anggota[0];
