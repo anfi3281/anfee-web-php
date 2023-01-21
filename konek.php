1d28f5c03cfb0ac6876ea4222894924a23c820bb
anfi3281 <anfi3281@gmail.com>
2023-01-21 17:34:36 +0700

    konekphp

---
+++ konek.php
@@ -0,0 +1,14 @@
+<?php
+$host = mysqli_connect("localhost", "root", "");
+if($host){
+    echo "Koneksi host berhasil.<br/>";
+}else{
+    echo "Koneksi gagal.<br/>";
+}
+
+$db = mysqli_select_db($host,"konekphp");
+if($db){
+    echo "Koneksi database berhasil.";
+}else{
+    echo "Koneksi database gagal.";
+}
