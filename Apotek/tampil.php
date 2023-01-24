40d32d71ca9dbd6d113845b95fc7ff637dd1b0c7
anfi3281 <anfi3281@gmail.com>
2023-01-24 21:02:07 +0700

    apotek

---
+++ Apotek/tampil.php
@@ -0,0 +1,30 @@
+<div class="kotak">        
+    <table border="1">
+      <tr>
+        <th>ID Obat</th>
+        <th>Nama Obat</th>
+        <th>ID Jenis Obat</th>
+        <th>Kategori Obat</th>
+        <th>Harga Jual Obat</th>
+        <th>Harga Beli Obat</th>
+        <th>Status Nama Obat</th>
+        <th>Keterangan</th>
+      </tr>
+      <tr>
+        <td>ID Obat</td>
+        <td>Nama Obat</td>
+        <td>ID Jenis Obat</td>
+        <td>Kategori Obat</td>
+        <td>Harga Jual Obat</td>
+        <td>Harga Beli Obat</td>
+        <td>Status Nama Obat</td>
+        <td><a class="ahapus" href="hapus.php">HAPUS</a> | <a class="aubah" href="ubah.php">UBAH</a></td>
+      </tr>
+    </table>
+  </div>
+  <form action="">
+    <table>
+      <input class="submit" type="submit" value="INPUT" />
+    </table>
+  </form>
+  
