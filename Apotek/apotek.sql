bec7bbbcdfd90f9f7fded550a7942beba280bf34
Randi Afif <91402751+anfi3281@users.noreply.github.com>
2023-01-24 18:35:00 +0700

    apotek

---
+++ Apotek/apotek.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: phpmyadmin.test
+-- Waktu pembuatan: 24 Jan 2023 pada 12.32
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apotek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbobat`
+--
+
+CREATE TABLE `tbobat` (
+  `kd_barang` varchar(5) NOT NULL,
+  `nm_barang` varchar(50) NOT NULL,
+  `kd_satuan` varchar(10) NOT NULL,
+  `kd_kategori` varchar(10) NOT NULL,
+  `hrg_jual` int(11) NOT NULL,
+  `hrg_beli` int(11) NOT NULL,
+  `active` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbobat`
+--
+ALTER TABLE `tbobat`
+  ADD PRIMARY KEY (`kd_barang`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
