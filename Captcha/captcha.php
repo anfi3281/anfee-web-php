295d4bb437bb70eba2ef1233a8ae604cd3d4c448
anfi3281 <anfi3281@gmail.com>
2023-01-24 10:49:34 +0700

    Session Captcha

--- Captcha/captcha.php
+++ Captcha/captcha.php
@@ -3,27 +3,27 @@ session_start();
 
 header("Content-type: image/png");
 
-$_SESSION["Captcha"]="";
+$_SESSION['Captcha'] = "";
 
 $gbr = imagecreate(200, 50);
-
 imagecolorallocate($gbr, 69, 179, 157);
 
 $color = imagecolorallocate($gbr, 253, 252, 252);
-$font = "/home/pc/All About Programming/anfee-web-php/Captcha/Allura-Regular.otf";
+$font = "Allura-Regular.otf";
 $ukuran_font = 20;
 $posisi = 32;
 
-for($i = 0; $i<=5;$i++){
-    $angka = rand(0,9);
-
-    $_SESSION["Captcha"].=$angka;
+for ($i = 0; $i <= 5; $i++) {
+    $angka = rand(0, 9);
+    
+    $_SESSION['Captcha'] .= $angka;
 
     $kemiringan = rand(20, 20);
 
-    imagettftext($gbr, $ukuran_font, $kemiringan, 8+15*$i, $posisi, $color, $font, $angka);    
-}
+    imagettftext($gbr, $ukuran_font, $kemiringan, 8 + 15 * $i, $posisi, $color, $font, $angka);
+    
+    
 
+}
 imagepng($gbr);
 imagedestroy($gbr);
-?>
---
+++ ITNSA/captcha.php
@@ -0,0 +1,28 @@
+<?
+session_start();
+
+header("Content-type: image/png");
+
+$_SESSION["Captcha"] = "";
+
+$gbr = imagecreate(200, 50);
+
+imagecolorallocate($gbr, 69, 179, 157);
+
+$color = imagecolorallocate($gbr, 253, 252, 252);
+$font = "roboto";
+$ukuran_font = 20;
+$posisi = 32;
+
+for ($i = 0; $i <= 6; $i++) {
+    $angka = rand(0, 9);
+
+    $_SESSION["Capctcha"] .= $angka;
+
+    $kemiringan = rand(20, 20);
+
+    imagettftext($gbr, $ukuran_font, $kemiringan, 8+15*$i, $posisi, $color, $font, $angka);
+}
+
+imagepng($gbr);
+imagedestroy($gbr);
