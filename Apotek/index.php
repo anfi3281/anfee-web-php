6c2e9517b683c88af1e0174da2f4c31fb4d024e5
anfi3281 <anfi3281@gmail.com>
2023-01-26 15:12:38 +0700

    apotek

--- Apotek/index.php
+++ Apotek/index.php
@@ -1,19 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Apotek</title>
-    <link rel="stylesheet" href="style.css" type="text/css" />
-  </head>
-  <body>
-    <div class="container">
-      <h1>DAFTAR OBAT</h1>
-      <h2>Ubah Obat</h2>
-      <?php
-      require("ubah.php");
-      ?>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Apotek</title>
+  <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+  <?php
+  require("koneksi.php");
+  session_start();
+  $_SESSION['a'] = "";
+  $_SESSION['b'] = "";
+  $_SESSION['c'] = "";
+  $_SESSION['d'] = "";
+  $_SESSION['e'] = "";
+  $_SESSION['f'] = "";
+  $_SESSION['g'] = "";
+
+
+
+  if (isset($_POST['ubah'])) {
+    $kbid = $_SESSION['a'];
+    $kb = $_POST['kb'];
+    $nb = $_POST['nb'];
+    $ks = $_POST['ks'];
+    $kk = $_POST['kk'];
+    $hj = $_POST['hj'];
+    $hb = $_POST['hb'];
+    $a = $_POST['a'];
+    $query = "UPDATE tbobat set kd_barang='$kb', nm_barang='$nb', kd_satuan='$ks', kd_kategori='$kk', hrg_jual='$hj', hrg_beli='$hb', active='$a' WHERE kd_barang='$kbid'";
+    mysqli_query($host, $query);
+    header("Location: index.php");
+  }
+
+  if (isset($_GET['ubah'])) {
+    $_SESSION['a'] = $_GET['a'];
+    $_SESSION['b'] = $_GET['b'];
+    $_SESSION['c'] = $_GET['c'];
+    $_SESSION['d'] = $_GET['d'];
+    $_SESSION['e'] = $_GET['e'];
+    $_SESSION['f'] = $_GET['f'];
+    $_SESSION['g'] = $_GET['g'];
+  }
+  ?>
+  <h1>DAFTAR OBAT</h1>
+  <div class="container">
+    <div class="kotakan">
+      <div class="daftar">
+        <h2>Tabel Obat</h2>
+        <?php
+        require("tampil.php");
+        ?>
+      </div>
+      <div class="isi">
+        <h2>Kelola Data Obat</h2>
+        <?php
+        require("kelola.php");
+        ?>
+      </div>
+
     </div>
-  </body>
+  </div>
+</body>
+
 </html>
--- Apotek/tampil.php
+++ Apotek/tampil.php
@@ -1,30 +1,34 @@
-<div class="kotak">        
-    <table border="1">
+<div class="kotak">
+  <table border="1" align="center">
+    <tr>
+      <th>ID Obat</th>
+      <th>Nama Obat</th>
+      <th>ID Jenis Obat</th>
+      <th>Kategori Obat</th>
+      <th>Harga Jual Obat</th>
+      <th>Harga Beli Obat</th>
+      <th>Status Nama Obat</th>
+      <th>Keterangan</th>
+    </tr>
+    <?php
+    require("koneksi.php");
+    $query = "SELECT * FROM tbobat ORDER BY kd_barang ASC";
+    $data = mysqli_query($host, $query);
+    while ($obat = mysqli_fetch_array($data)) {
+    ?>
       <tr>
-        <th>ID Obat</th>
-        <th>Nama Obat</th>
-        <th>ID Jenis Obat</th>
-        <th>Kategori Obat</th>
-        <th>Harga Jual Obat</th>
-        <th>Harga Beli Obat</th>
-        <th>Status Nama Obat</th>
-        <th>Keterangan</th>
+        <td><?php echo $obat[0]; ?></td>
+        <td><?php echo $obat[1]; ?></td>
+        <td><?php echo $obat[2]; ?></td>
+        <td><?php echo $obat[3]; ?></td>
+        <td><?php echo $obat[4]; ?></td>
+        <td><?php echo $obat[5]; ?></td>
+        <td><?php echo $obat[6]; ?></td>
+        <td><a class="ahapus" href="kelola.php?hapus=iya&koba=<?php echo $obat[0]; ?>">HAPUS</a> | <a class="aubah" href="index.php?ubah=iya&a=<?php echo $obat[0]; ?>&b=<?php echo $obat[1]; ?>&c=<?php echo $obat[2]; ?>&d=<?php echo $obat[3]; ?>&e=<?php echo $obat[4]; ?>&f=<?php echo $obat[5]; ?>&g=<?php echo $obat[6]; ?>">UBAH</a></td>
       </tr>
-      <tr>
-        <td>ID Obat</td>
-        <td>Nama Obat</td>
-        <td>ID Jenis Obat</td>
-        <td>Kategori Obat</td>
-        <td>Harga Jual Obat</td>
-        <td>Harga Beli Obat</td>
-        <td>Status Nama Obat</td>
-        <td><a class="ahapus" href="hapus.php">HAPUS</a> | <a class="aubah" href="ubah.php">UBAH</a></td>
-      </tr>
-    </table>
-  </div>
-  <form action="">
-    <table>
-      <input class="submit" type="submit" value="INPUT" />
-    </table>
-  </form>
-  
+    <?php
+    }
+    ?>
+  </table>
+</div>
+</form>
